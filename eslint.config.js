a2a5a399c356f4fdbf13b31138da4577b9805957
vitaliy1990 <Chumakov_vitaliy@meta.ua>
2025-07-04 16:09:39 +0300

    add settings prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,103 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import prettierConfig from "eslint-config-prettier";
 
-export default tseslint.config([
-  globalIgnores(['dist']),
+// PLUGINS //
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import prettierPlugin from "eslint-plugin-prettier";
+import react from "eslint-plugin-react";
+import unusedImports from "eslint-plugin-unused-imports";
+import eslintPluginImport from "eslint-plugin-import";
+
+export default tseslint.config(
+  { ignores: ["dist"] },
   {
-    files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
+      ...tseslint.configs.recommended,
+      prettierConfig,
     ],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
-  },
-])
+    plugins: {
+      react,
+      "unused-imports": unusedImports,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      import: eslintPluginImport,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      "prettier/prettier": "error",
+      "react/jsx-max-props-per-line": ["error", { maximum: 1 }],
+      "react/jsx-first-prop-new-line": ["error", "multiline"],
+      "@typescript-eslint/no-unused-vars": "off",
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "error",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: [
+            ["builtin", "external"],
+            ["internal"],
+            ["parent", "sibling", "index"],
+          ],
+          pathGroups: [
+            {
+              pattern: "react",
+              group: "builtin",
+              position: "before",
+            },
+            {
+              pattern: "components",
+              group: "internal",
+            },
+            {
+              pattern: "common",
+              group: "internal",
+            },
+            {
+              pattern: "routes/ **",
+              group: "internal",
+            },
+            {
+              pattern: "assets/**",
+              group: "internal",
+              position: "after",
+            },
+          ],
+          pathGroupsExcludedImportTypes: ["internal"],
+          "newlines-between": "always",
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true,
+            orderImportKind: "asc",
+          },
+        },
+      ],
+
+      "import/named": "off",
+      "import/namespace": "error",
+      "import/default": "error",
+      "import/export": "error",
+    },
+  }
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage";
-import ErrorBoundary from "./components/ErrorBoundary/ErrorBoundary";
-import GenresProvider from "./components/GenresProvider/GenresProvider";
-import { useGenres } from "./hooks/useGenres";
-import Loader from "./components/Loader/Loader";
-import { Toaster } from "react-hot-toast";
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import ErrorBoundary from './components/ErrorBoundary/ErrorBoundary';
+import GenresProvider from './components/GenresProvider/GenresProvider';
+import Loader from './components/Loader/Loader';
+import { useGenres } from './hooks/useGenres';
+import HomePage from './pages/HomePage';
 
 const AppContainer = () => {
   const { isLoading } = useGenres();
 
   if (isLoading) {
-    return <Loader rootClassName="h-dvh" />;
+    return <Loader rootClassName='h-dvh' />;
   }
 
   return (
-    <div className="max-w-[1200px] mx-auto p-4 md:p-8 flex flex-col gap-8">
-      <Toaster position="top-center" />
+    <div className='mx-auto flex max-w-[1200px] flex-col gap-8 p-4 md:p-8'>
+      <Toaster position='top-center' />
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route
+          path='/'
+          element={<HomePage />}
+        />
+        <Route
+          path='*'
+          element={
+            <Navigate
+              to='/'
+              replace
+            />
+          }
+        />
       </Routes>
     </div>
   );
